645b2dc48892f49b7deab71f21d3607a781944e5
faisaliqbalkhattak <fiqbal.bese23seecs@seecs.edu.pk>
2026-02-22 15:18:45 +0500

    added the metch routes to get and post the matches structures

---
+++ drizzle/0001_odd_swordsman.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "matches" ADD COLUMN "end_time" timestamp with time zone NOT NULL;
--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { desc } from 'drizzle-orm';
 import { db } from '../db/db.js';
 import { matches } from '../db/schema.js';
 import { createMatchSchema, listMatchesQuerySchema } from '../validation/matches.js';
 import { getMatchStatus } from '../utils/match-status.js';
+import { json } from 'zod';
 
 export const matchRouter = Router();
 
@@ -11,7 +13,7 @@ export const matchRouter = Router();
 matchRouter.get('/', async (req, res) => {
     const parsed = listMatchesQuerySchema.safeParse(req.query);
     if (!parsed.success) {
-        return res.status(400).json({ errors: parsed.error.format() });
+        return res.status(400).json({ errors: parsed.error.format(), details : json.stringify(parsed.error) });
     }
 
     const limit = parsed.data.limit ?? 50;
@@ -20,13 +22,13 @@ matchRouter.get('/', async (req, res) => {
         const rows = await db
             .select()
             .from(matches)
-            .orderBy(matches.startTime.desc())
+            .orderBy(desc(matches.startTime))
             .limit(limit);
 
         return res.status(200).json({ data: rows });
     } catch (e) {
         console.error('Failed to fetch matches:', e);
-        return res.status(500).json({ error: 'Failed to fetch matches' });
+        return res.status(500).json({ error: 'Failed to fetch matches', details : json.stringify(e)  });
     }
 });
 
@@ -34,7 +36,7 @@ matchRouter.post('/', async (req, res) => {
     const parsed = createMatchSchema.safeParse(req.body);
 
     if (!parsed.success) {
-        return res.status(400).json({ errors: parsed.error.format() });
+        return res.status(400).json({ errors: parsed.error.format() , details : json.stringify(parsed.error) });
     }
 
     const { homeTeam, awayTeam, sport, startTime, endTime, homeScore, awayScore } = parsed.data;
@@ -57,6 +59,6 @@ matchRouter.post('/', async (req, res) => {
         res.status(201).json({ data: event });
     } catch (e) {
         console.error('Failed to create match:', e);
-        res.status(500).json({ error: 'Failed to create the match' });
+        res.status(500).json({ error: 'Failed to create the match', details : json.stringify(e)  });
     }
 });
