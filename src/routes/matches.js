a57c5d26dedd73fc1c378d131cf3fd135700374f
faisaliqbalkhattak <fiqbal.bese23seecs@seecs.edu.pk>
2026-02-22 15:35:28 +0500

    added the metch routes to get and post the matches structures

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -4,7 +4,6 @@ import { db } from '../db/db.js';
 import { matches } from '../db/schema.js';
 import { createMatchSchema, listMatchesQuerySchema } from '../validation/matches.js';
 import { getMatchStatus } from '../utils/match-status.js';
-import { json } from 'zod';
 
 export const matchRouter = Router();
 
@@ -13,7 +12,7 @@ export const matchRouter = Router();
 matchRouter.get('/', async (req, res) => {
     const parsed = listMatchesQuerySchema.safeParse(req.query);
     if (!parsed.success) {
-        return res.status(400).json({ errors: parsed.error.format(), details : json.stringify(parsed.error) });
+        return res.status(400).json({ errors: parsed.error.format(), details : JSON.stringify(parsed.error) });
     }
 
     const limit = parsed.data.limit ?? 50;
@@ -28,7 +27,7 @@ matchRouter.get('/', async (req, res) => {
         return res.status(200).json({ data: rows });
     } catch (e) {
         console.error('Failed to fetch matches:', e);
-        return res.status(500).json({ error: 'Failed to fetch matches', details : json.stringify(e)  });
+        return res.status(500).json({ error: 'Failed to fetch matches', details : JSON.stringify(e)  });
     }
 });
 
@@ -36,7 +35,7 @@ matchRouter.post('/', async (req, res) => {
     const parsed = createMatchSchema.safeParse(req.body);
 
     if (!parsed.success) {
-        return res.status(400).json({ errors: parsed.error.format() , details : json.stringify(parsed.error) });
+        return res.status(400).json({ errors: parsed.error.format() , details : JSON.stringify(parsed.error) });
     }
 
     const { homeTeam, awayTeam, sport, startTime, endTime, homeScore, awayScore } = parsed.data;
@@ -59,6 +58,6 @@ matchRouter.post('/', async (req, res) => {
         res.status(201).json({ data: event });
     } catch (e) {
         console.error('Failed to create match:', e);
-        res.status(500).json({ error: 'Failed to create the match', details : json.stringify(e)  });
+        res.status(500).json({ error: 'Failed to create the match', details : JSON.stringify(e)  });
     }
 });
